8881db6e13f533917467d842a4c6438a2d3eab66
admin <admin@MacMini.local>
2014-07-29 21:08:08 +0800

    Added product collection view

---
+++ HelloPOS/JKLABProductViewController.h
@@ -0,0 +1,13 @@
+//
+//  JKLABProductViewController.h
+//  HelloPOS
+//
+//  Created by admin on 29/7/14.
+//  Copyright (c) 2014 jklab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKLABProductViewController : UICollectionViewController<UICollectionViewDataSource>
+
+@end
---
+++ HelloPOS/JKLABProductViewController.m
@@ -0,0 +1,67 @@
+//
+//  JKLABProductViewController.m
+//  HelloPOS
+//
+//  Created by admin on 29/7/14.
+//  Copyright (c) 2014 jklab. All rights reserved.
+//
+
+#import "JKLABProductViewController.h"
+
+@interface JKLABProductViewController ()
+
+@end
+
+@implementation JKLABProductViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"ProductCell" forIndexPath:indexPath];
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 30;
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
