980280fb2755ecf7f12ebe2f69ddde49eb3717d9
jackkwong <abc@abcs-MacBook.local>
2014-07-30 01:15:44 +0800

    Added product cell and sample product images

---
+++ HelloPOS/JKLABProductCell.h
@@ -0,0 +1,14 @@
+//
+//  JKLABProductCell.h
+//  HelloPOS
+//
+//  Created by abc on 30/7/14.
+//  Copyright (c) 2014 jklab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKLABProductCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *uiProductImageView;
+
+@end
---
+++ HelloPOS/JKLABProductCell.m
@@ -0,0 +1,31 @@
+//
+//  JKLABProductCell.m
+//  HelloPOS
+//
+//  Created by abc on 30/7/14.
+//  Copyright (c) 2014 jklab. All rights reserved.
+//
+
+#import "JKLABProductCell.h"
+
+@implementation JKLABProductCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
